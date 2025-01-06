9218b210724bd03d9ae1c63ac9d82849159c1957
Neel Sharma <nsdj.sharma@gmail.com>
2025-01-06 19:25:05 +1100

    use PositionDutyCycle in AlgaeArm for familiarity purposes
    like, who would prefer voltage over -1 to 1?

--- src/main/java/frc/robot/constants/AlgaeArmConstants.java
+++ src/main/java/frc/robot/constants/AlgaeArmConstants.java
@@ -2,11 +2,8 @@ package frc.robot.constants;
 
 import static edu.wpi.first.units.Units.Degrees;
 
-import java.util.List;
-
 import com.ctre.phoenix6.signals.InvertedValue;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 
@@ -16,19 +13,19 @@ public class AlgaeArmConstants {
 
   public static final int MOTOR_ID = 3;
 
-  //public static final List<Angle> PIVOT_ANGLES = List.of(
-  //    Degrees.of(0), Degrees.of(35), Degrees.of(200), Degrees.of(220));
+  // public static final List<Angle> PIVOT_ANGLES = List.of(
+  // Degrees.of(0), Degrees.of(35), Degrees.of(200), Degrees.of(220));
 
-  public static class ANGLES{
-    public static Angle STOW = Degrees.of(0); 
-    public static Angle LOAD_START = Degrees.of(35); 
-    public static Angle LOAD_END = Degrees.of(45); 
-    public static Angle UNLOAD = Degrees.of(-35); 
+  public static class ANGLES {
+    public static Angle STOW = Degrees.of(0);
+    public static Angle LOAD_START = Degrees.of(35);
+    public static Angle LOAD_END = Degrees.of(45);
+    public static Angle UNLOAD = Degrees.of(-35);
   }
 
   public static final InvertedValue MOTOR_INVERTED = InvertedValue.CounterClockwise_Positive;
 
-  public static final double PID_P = 2;
+  public static final double PID_P = 0.1;
   public static final double PID_I = 0;
   public static final double PID_D = 0;
   public static final double PID_FF = 0;
