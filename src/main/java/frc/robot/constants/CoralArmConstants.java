0ae85b9f89ddda6ad84b2daca111e616ff1ca01f
roterrex <joel.roterex@gmail.com>
2025-01-06 18:02:42 +1100

    fixed coral constants for conisist

--- src/main/java/frc/robot/constants/CoralArmConstants.java
+++ src/main/java/frc/robot/constants/CoralArmConstants.java
@@ -1,7 +1,10 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Degree;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.units.measure.Angle;
 import frc.robot.utils.EncoderSupplier;
 import frc.robot.utils.motorsupplier.MotorSupplier;
 import frc.robot.utils.motorsupplier.TalonMotorSupplier;
@@ -10,10 +13,16 @@ public class CoralArmConstants {
   protected CoralArmConstants() {
   }
 
+  public static class ANGLES {
+    public static Angle STOW = Degree.of(90);
+    public static Angle LOAD = Degree.of(35);
+    public static Angle UNLOAD = Degree.of(-35);
+  }
+
   // note: none of these values are real
   public static final MotorSupplier<WPI_TalonSRX> MOTOR_ID = new TalonMotorSupplier(3).withBrake();
   public static final EncoderSupplier ENCODER_ID = new EncoderSupplier(new int[] { 0, 1 }, 0.002);
-  public static final double[] ANGLES_DEGREES = new double[] { 0, 35, 180 };
+  //public static final double[] ANGLES_DEGREES = new double[] { 0, 35, 180 };
 
   public static final double PID_P = 2;
   public static final double PID_I = 0;
